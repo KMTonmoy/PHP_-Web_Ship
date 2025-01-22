32643f66251e7e08764e3aef01ac6490596cad72
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-22 10:37:28 +0600

    Comment added

---
+++ class-05/index.php
@@ -0,0 +1,11 @@
+<?php
+// This Is Our Product Priceing Section
+
+
+$first_Price = 100; //storeing first price
+$second_Price = 200; //storeing section price
+$toalt_Price = $first_Price + $second_Price;
+
+echo "Total Price is = ". $toalt_Price //calculate total price
+
+?>
