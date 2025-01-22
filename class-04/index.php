7a22b906bdc791df93f57266b919f236c510542d
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-22 10:32:59 +0600

    PHP Class added

---
+++ class-04/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $firstName = "Tonmoy";
+    $income = 500000000;
+    ?>
+
+    <h1>Hello <?php echo "Hello World 🌍🌎🌏"; ?> </h1>
+    <h2>My Name is <?php echo $firstName ?></h2>
+    <h2>My Total Income is : <?php echo $income ?></h2>
+
+</body>
+
+</html>
