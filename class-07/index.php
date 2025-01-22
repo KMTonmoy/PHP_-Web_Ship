d33f81918d5a38a0d52657a87fda6fba42f67bab
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-22 10:55:00 +0600

    Class 07 added

---
+++ class-07/index.php
@@ -0,0 +1,12 @@
+<?php
+//echo "Tonmoy Ahamed"; //Faster Then The Print
+// define('isStudent' , true);
+// We Should Use Echo Cause it is  More Faster then The Print !!! Soo We have to use Echo
+// print isStudent
+
+$name = "Tonmoy";
+//Same Thing But Some Different In the Typeing 
+echo "<h1>". $name . "</h1>"; 
+echo "<h1>$name</h1>"  
+ 
+?>
