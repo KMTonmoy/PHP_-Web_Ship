f7bf9d0da01f2640113e261c17ab831742a8a604
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-22 10:45:48 +0600

    PHP const add

---
+++ class-06/index.php
@@ -0,0 +1,11 @@
+<?php
+// Normal variable Any One can change form this file any line of code 
+$name = "Tonmoy";
+
+// A Conostent Any one cant chnage or reuse again the same name is imposible
+define("Name", "Tonmoy");
+define("amount", 420);
+
+
+
+echo Name. amount;
