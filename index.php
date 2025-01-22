17f30829baed34e4b83107c8ec5ffdd9817fa2d1
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-22 10:24:20 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "Hello World";
+    echo "</br>";
+    $name = "Tonmoy";
+    $isStudent = true;
+    $age = 15;
+    $income = 500;
+
+    echo "My Name Is $name 👦" . " " . "My i am $age 👴 years old." . " " . "I earn In a month or my pocket money is $income 💸💸";
+    ?>
+</body>
+
+</html>
